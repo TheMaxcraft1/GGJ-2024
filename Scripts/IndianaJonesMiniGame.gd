6d5584a67742aa873a4b8b6ee79e11a38a401fc7
Máximo Fain <maximofain0@gmail.com>
2024-01-26 19:45:16 -0300

    Indiana Jones mg update

--- Scripts/IndianaJonesMiniGame.gd
+++ Scripts/IndianaJonesMiniGame.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 var player_win : bool
-
+var minigame_started : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var time = $Timer.wait_time
+	$Rock.play('start_anim')
+	$AnimationPlayer.play('start_anim')
+	$HUD/TimerLabel.set_visible(false)
+	await get_tree().create_timer(1).timeout
+	$HUD/TimerLabel.set_visible(true)
+	minigame_started = true
+	$Rock.play("default")
+	var time = $MGTimer.wait_time
+	$MGTimer.start()
 	$AnimationPlayer.speed_scale = 1 / time # dividing 1 which is the animation lenght by the timer's wait time
 	$AnimationPlayer.play("ball_ind_jones_escaping_anim")
 
@@ -13,25 +21,44 @@ func _ready():
 func _process(delta):
 	checkLose()
 	
-	$ProgressBar.value += -(delta * 25)
+	if minigame_started:
+		$ProgressBar.value += -(delta * 50)
 	
-	if Input.is_action_just_pressed("Interact"):
-		$ProgressBar.value += (delta * 500)
+		if Input.is_action_just_pressed("Interact"):
+			$ProgressBar.value += (delta * 500)
 	
-	$HUD/TimerLabel.text = str(ceil($Timer.time_left)) + " sec"
+	$HUD/TimerLabel.text = str(ceil($MGTimer.time_left)) + " sec"
+	
+	if minigame_started:
+		if $ProgressBar.value > (100/3)*2:
+			$IndJonesPlayer.texture = preload("res://Textures/IndianaJonesMiniGame/IndianaJonesSpriteSheet (1).png")
+		elif $ProgressBar.value <= (100/3)*2 and $ProgressBar.value > 100/3:
+			$IndJonesPlayer.texture = preload("res://Textures/IndianaJonesMiniGame/IndianaJonesSpriteSheet (3).png")
+		else:
+			$IndJonesPlayer.texture = preload("res://Textures/IndianaJonesMiniGame/IndianaJonesSpriteSheet (4).png")
+	
 	
 	if player_win == true: # go into a transition
-		GAMEMNGR.score += 1
 		player_win = false
+		minigame_started = false
+		$Rock.play("start_anim")
+		$AnimationPlayer.speed_scale = 1
+		$AnimationPlayer.play("end_anim1")
+		GAMEMNGR.score += 1
 		GAMEMNGR.ind_jones += 1
+		await get_tree().create_timer(5).timeout
 		FADE_EFFECT.scene_transition("res://Scenes/TransitionNode.tscn")
 	
 
-func _on_timer_timeout():
-	if not $ProgressBar.value == 0:
-		player_win = true
-
-
 func checkLose():
 	if $ProgressBar.value == 0:
+		minigame_started = false
+		$HUD/TimerLabel.set_visible(false)
+		$MGTimer.stop()
 		player_win = false
+		$AnimationPlayer.pause()
+
+
+func _on_mg_timer_timeout():
+	if not $ProgressBar.value == 0:
+		player_win = true
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,17 +2,11 @@ extends Control
 
 
 func _on_play_pressed(): #Boton de Play
-	pass # Replace with function body.
-
-
-
+	FADE_EFFECT.scene_transition('res://Scenes/IndianaJonesMiniGame.tscn')
+	
 
 func _on_options_pressed(): #Boton Options
 	get_tree().change_scene_to_file("res://Scenes/OptionsMenu.tscn")
-	
-	
-
-
 
 
 func _on_exit_pressed(): #Boton Exit
